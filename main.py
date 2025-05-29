9e8b9364fd1111b2e01bfbeaaa55ff366382fb21
OlekOlekOlekOlek <dsw55813@student.dsw.edu.pl>
2025-05-29 21:02:58 +0200

    Task7:zapis danych z obiektu do pliku .xml

--- main.py
+++ main.py
@@ -5,9 +5,46 @@ import sys
 import yaml
 import xml.etree.ElementTree as ET
 
+def dict_to_xml(tag: str, d) -> ET.Element:
+    elem = ET.Element(tag)
+
+
+    if not isinstance(d, dict):
+        elem.text = str(d)
+        return elem
+
+    for key, val in d.items():
+        if isinstance(val, dict):
+            child = dict_to_xml(key, val)
+            elem.append(child)
+        elif isinstance(val, list):
+            for item in val:
+                child = dict_to_xml(key, item)
+                elem.append(child)
+        else:
+            child = ET.SubElement(elem, key)
+            child.text = str(val)
+    return elem
+
+def write_xml(data: dict, file_path: str) -> bool:
+    try:
+        if len(data) != 1:
+            print("Dane do zapisu XML powinny mieć dokładnie jeden klucz - nazwę głównego elementu.")
+            return False
+        root_tag = list(data.keys())[0]
+        root_elem = dict_to_xml(root_tag, data[root_tag])
+
+        tree = ET.ElementTree(root_elem)
+        tree.write(file_path, encoding="utf-8", xml_declaration=True)
+        print(f"Dane zapisane do pliku XML: {file_path}")
+        return True
+    except Exception as e:
+        print(f"Błąd zapisu XML: {e}")
+        return False
+
 def read_xml(file_path: str) -> ET.Element | None:
     try:
-        tree = ET.parse(file_path)  # próba sparsowania XML
+        tree = ET.parse(file_path)
         root = tree.getroot()
         print("Plik XML poprawny.")
         return root
@@ -30,7 +67,6 @@ def read_yaml(file_path: str) -> dict | None:
     return None
 
 def write_yaml(data: dict, file_path: str) -> bool:
-    """Zapisuje dane do pliku YAML."""
     try:
         with open(file_path, "w", encoding="utf-8") as f:
             yaml.dump(data, f, allow_unicode=True, sort_keys=False)
@@ -101,6 +137,9 @@ def main():
         write_json(data, args.target)
     elif ext_out in [".yaml", ".yml"]:
         write_yaml(data, args.target)
+    elif ext_out == ".xml":
+        write_xml(data, args.target)
+    
     else:
         print("Obsługiwane wyjścia to tylko .json i .yaml")
         sys.exit(1)
